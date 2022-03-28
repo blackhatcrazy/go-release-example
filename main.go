cef9ff01f90c7cd6a85d5532e8c964ca4d42414c
Alex Howard <ahoward1024@gmail.com>
2022-03-28 16:49:51 -0600

    feat!: add required message of the day (#5)
    BREAKING CHANGE: A message of the day flag is now required to invoke the
    app correctly. Use `--motd="<your message>"` when running.
    
    * fix: parse flags in main for tests to pass

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+var motd string
 
 func PrintfFix(message string) {
 	fmt.Printf("fix: %s\n", message)
@@ -15,8 +21,18 @@ func PrintfBreakingChange(ty string, message string) {
 }
 
 func main() {
-	fmt.Println("Hello world!")
+	flag.Parse()
+	if motd == "" {
+		fmt.Println("Error: Message of the day cannot be empty.")
+		os.Exit(1)
+	}
+	fmt.Printf("%s\n", motd)
 	PrintfFeature("This is some more text!")
 	PrintfFeature("This is the third line of text")
 	PrintfFix("Fixed typo in third line of text")
+	PrintfBreakingChange("feat", "Add required message of the day")
+}
+
+func init() {
+	flag.StringVar(&motd, "motd", "", "Message of the day to print at startup")
 }
